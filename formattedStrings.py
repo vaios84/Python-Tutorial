74a0c4ec9c8ad67ad287db3ca9854bc06648e465
vaios stergiopoulos <software.vast@gmail.com>
2020-07-21 01:04:55 +0300

    get index of strings

---
+++ formattedStrings.py
@@ -0,0 +1,11 @@
+first = 'Jack'
+last = 'Russel'
+
+message = '[' + first + ']' + ' [' + last + ']' + ' is a coder!' 
+
+print(message)
+
+#formatted strings like below only in python 3 
+lang = 'Python'
+pos = 1
+print(f'This {lang} language is at {pos} position since 2018.')
--- getInput.py
+++ getInput.py
@@ -10,11 +10,10 @@ print(price + price2)
 num = input('\nEnter a number: ')
 print(num)
 
-#Try using raw_input instead of input for strings
-onoma = raw_input('\nEnter name: ') 
+onoma = input('\nEnter name: ') 
 print('hello ' + onoma + ' my friend')
 
-# input and raw_input always takes input as a string example '1982' not 1982
+# input always takes input as a string example '1982' not 1982
 birth_date= input('\nbirth date: ')
 age = 2020 - int(birth_date)
 print('age: '  + str(age))
