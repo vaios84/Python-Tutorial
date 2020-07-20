f7340aad0591e00a50797e7248515732c58135ce
vaios stergiopoulos <software.vast@gmail.com>
2020-07-21 00:30:11 +0300

    get index of strings

---
+++ print-index-of-string.py
@@ -0,0 +1,25 @@
+course = 'The total Python Course'
+
+print(course)
+print(course[0])
+print(course[1])
+print(course[2])
+
+print(course[4:9])
+
+print(course[-6])
+print(course[-5])
+print(course[-4])
+print(course[-3])
+print(course[-2])
+print(course[-1])
+
+print('\n################\n')
+
+anotherString = course[10:16]
+print(anotherString)
+
+
+print('\n################\n')
+
+print(course[1:-1])
