5ecf36c3c0a76b2fd78adb1c514f4cfcdcecfbbf
vaios stergiopoulos <software.vast@gmail.com>
2020-07-20 23:57:15 +0300

    new commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+print('hello world! \n')
+print('vaios goes python!\n')
+
+#multi line text
+bigText = ''' 
+Hello all, 
+Hope everything is fine. I am coming tomorrow.  
+
+Kisses,
+
+Vaios
+ '''
+ 
+ 
+print(bigText)
