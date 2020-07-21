69916158095d413c8343f101d2c565bc31ca5634
vaios stergiopoulos <software.vast@gmail.com>
2020-07-21 22:58:21 +0300

    get index of strings

--- hello.py
+++ hello.py
@@ -10,6 +10,5 @@ Kisses,
 
 Vaios
  '''
- 
- 
+
 print(bigText)
---
+++ string-functions.py
@@ -0,0 +1,22 @@
+newCourse = 'Python for The Absolute Beginners'
+print(len(newCourse))
+
+
+print(newCourse.lower())
+
+print(newCourse.upper())
+
+print(newCourse.find('B'))
+print(newCourse.find('for'))
+print(newCourse.find('Z'))
+
+print(newCourse.replace('Absolute Beginners', 'New Players'))
+
+print(newCourse.replace('P', 'J'))
+
+#check if a string exists in a text and print the result true or false
+print('Python' in newCourse)
+print('Falanji' in newCourse)
+
+
+
