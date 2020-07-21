e420e72517332497915eeddd9a2e4559ce1f3bc0
vaios stergiopoulos <software.vast@gmail.com>
2020-07-22 01:05:15 +0300

    data harvester

---
+++ if-statements.py
@@ -0,0 +1,28 @@
+isCold = True
+isHot = False
+
+if isCold:
+    print('It is a cold day...')
+    print('Drink somethink hot!')
+elif isHot:
+    print('This is a hot day...')
+    print('Drink lot of water!') 
+else:
+    print('Lovely day whatever...')
+
+print('Enjoy your day!\n')
+
+print('#################')
+
+
+highIncome= True
+goodCredit = True
+hasCriminalRecord = False
+
+if highIncome and goodCredit and not hasCriminalRecord:
+    print('Eligible for loan')
+else:
+    print('Not Eligible for loan')
+
+if highIncome or goodCredit
+    print('Eligible for small loan only!')
---
+++ math-functions.py
@@ -0,0 +1,12 @@
+import math
+
+x=2.89
+y=-50.911
+print(round(x))
+print(abs(y))
+
+x+=10
+print(math.ceil(x))
+print(math.floor(x))
+
+
