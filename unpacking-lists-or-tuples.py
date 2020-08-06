717112f9d4a01bbe2e66a0c37c854e2e522c4823
vaios stergiopoulos <software.vast@gmail.com>
2020-08-06 19:23:06 +0300

    new lesson

---
+++ unpacking-lists-or-tuples.py
@@ -0,0 +1,17 @@
+
+# A new tuple, it cannot be changed.
+# You can't add, remove or change any item. Like a constant.
+numbers = (7,5,1) 
+
+x, y, z = numbers #unpacking the values of numbers tuple
+print(x)
+print(y)
+print(z)
+
+coordinates = [10.25, 54.88, 124.56, 8.96] # a new List
+x, y, z, w = coordinates #unpacking the values 
+print(x)
+print(y)
+print(z)
+print(w)
+
