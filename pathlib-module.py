e84a174c55f6bfe6c30f8fae27dd250b224aed3c
vaios stergiopoulos <software.vast@gmail.com>
2020-08-19 17:45:25 +0300

    pathlib module

--- pathlib-module.py
+++ pathlib-module.py
@@ -1,2 +1,27 @@
-import pathlib
+from pathlib import Path
+
+# absolute path 
+# C:\Program Files\Microsoft\AppFolder
+# /usr/local/bin/myapp
+
+# relative path
+path1 = Path("ecommerce")
+if not path1.exists():
+	path1.mkdir() # create a folder named ecommerce
+	print("ecommerce folder created")
+else:
+	path1.rmdir()
+	print("ecommerce folder deleted")
+	
+	
+path2 = Path("anotherFolder")
+
+print(path1.exists())
+print(path2.exists())
+
+path3 = Path()
+print("\nsearch for all files .py")
+for file in path3.glob('*.py'):
+	print(file)
+
 
