8bed35168e50c33820f9a6ac0e2ce2fe48aba6a3
vaios stergiopoulos <software.vast@gmail.com>
2021-01-17 09:56:16 +0200

    work with txt files

---
+++ working-with-files/rename_multiple_files.py
@@ -0,0 +1,30 @@
+# Python3 code to rename multiple files in a directory or folder 
+
+# importing os module 
+import os 
+import os.path
+
+# Function to rename a .txt file
+def renameFile(filename):
+		changed_fname = filename[9:]  # remove first 9 chars 
+		file = pathlib.Path(filename)
+		if file.exists():
+			os.rename(filename, changed_fname) 
+		else:
+			print('file not found')
+
+
+for i in range(1,4):
+		renameFile('output-' + str(i) + '-cf-train-10-users.dat')
+		renameFile('output-' + str(i) + '-cf-train-1-users.dat')
+		renameFile('output-' + str(i) + '-cf-test-10-users.dat')
+		renameFile('output-' + str(i) + '-cf-test-1-users.dat')
+		renameFile('output-' + str(i) + '-cf-train-10-items.dat')
+		renameFile('output-' + str(i) + '-cf-train-1-items.dat')
+		renameFile('output-' + str(i) + '-cf-test-10-items.dat')
+		renameFile('output-' + str(i) + '-cf-test-1-items.dat') 
+
+
+
+
+
